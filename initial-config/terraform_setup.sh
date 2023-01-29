38150c9c7bbc3265c584f60471f6ad6330319eb2
zdravkov02 <115472721+zdravkov02@users.noreply.github.com>
2023-01-29 03:43:18 +0200

    Update terraform_setup.sh

--- initial-config/terraform_setup.sh
+++ initial-config/terraform_setup.sh
@@ -4,6 +4,6 @@ wget https://releases.hashicorp.com/terraform/1.3.7/terraform_1.3.7_linux_amd64.
 echo "* Unzip TF ... *"
 unzip /tmp/terraform.zip
 echo "* Move TF to PATH ... *"
-mv terraform /usr/local/bin/
+mv terraform /usr/bin/
 echo "* Remove TF archive ... *"
 rm /tmp/terraform.zip
